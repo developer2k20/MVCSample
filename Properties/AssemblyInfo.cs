bcf47a6fbcb4eafa32689731c79e77c8342992af
developer2k20 <developer.2k20@gmail.com>
2016-04-13 16:35:53 +0530

    Added files via upload

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("HRMS")]
+[assembly: AssemblyTitle("HRMS.Tests")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("HRMS")]
+[assembly: AssemblyProduct("HRMS.Tests")]
 [assembly: AssemblyCopyright("Copyright ©  2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("53cd3ca7-9337-4d8b-9e87-a757a522e730")]
+[assembly: Guid("491e73c3-eedd-430a-80df-ce40fa3b2e1f")]
 
 // Version information for an assembly consists of the following four values:
 //
