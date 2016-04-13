c09b0438e585e95388786760e4a5c25e098af693
developer2k20 <developer.2k20@gmail.com>
2016-04-13 16:29:39 +0530

    Added files via upload

---
+++ Controllers/AccountController.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using HRMS.Repository;
+using HRMS.Models;
+using System.Text;
+using System.Net;
+using System.Net.Mail;
+using System.IO;
+
+namespace HRMS.Controllers
+{
+    public class AccountController : Controller
+    {
+        //
+        // GET: /Account/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Login()
+        {
+            ModelState.Clear();
+
+            string UserName = Request.Form["UserName"];
+            string Password = Request.Form["Password"];
+
+            ViewBag.Title = "HRMS - User Login";
+            string name = ViewBag.ErrorMessage;
+            string name1 = ViewBag.Message;
+
+            if (UserName != null && UserName.Trim() != "" && Password != null && Password != "")
+            {
+                UserRepository ur = new UserRepository();
+
+                bool ValidUser = ur.CheckUserLogin(UserName, Password);
+
+                if (ValidUser)
+                    return RedirectToAction("GetAllEmpDetails", "Index");
+
+                TempData["Message"] = "";
+                TempData["ErrorMessage"] = "Invalid user name or password!";
+            }
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult AddUser()
+        {
+            ViewBag.Title = "HRMS - New User";
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult AddUser(UserModel usr)
+        {
+            ModelState.Clear();
+
+            string UserName = Request.Form["UserName"];
+            string Email = Request.Form["Email"];
+
+            ViewBag.Title = "HRMS - Add User";
+
+            if (UserName != null && UserName.Trim() != "")
+            {
+                SmtpClient sc = new SmtpClient();
+                sc.Host = "127.0.0.1";
+                StringBuilder sb = new StringBuilder();
+
+                var ss = RenderRazorViewToString("UserMailTemplate", usr);
+
+                sb.Append("<h3>Login Credentials</h3>");
+                sb.Append("<hr />");
+                sb.Append("<b>User Name:</b> " + UserName);
+                sb.Append("<b>Password:</b> ChangeIt123");
+
+                var mail = new MailMessage("developer.2k20@gmail.com", Email)
+                {
+                    Subject = "Login Credentials",
+                    Body = sb.ToString()
+                };
+
+                //sc.Send(mail);
+
+                TempData["Message"] = "User added successfully." + Environment.NewLine + "Password has been send to user's registered email id.";
+                TempData["ErrorMessage"] = "";
+
+                return RedirectToAction("Login");
+            }
+
+            return View();
+        }
+
+        public ActionResult CheckUserName(string UserName)
+        {
+            UserRepository usr = new UserRepository();
+
+            bool RetValue = usr.CheckDuplicateUserName(UserName);
+
+            return Json(!RetValue, JsonRequestBehavior.AllowGet);
+        }
+
+
+        private string RenderRazorViewToString(string viewName, object model)
+        {
+            ViewData.Model = model;
+            using (var sw = new StringWriter())
+            {
+                var viewResult = ViewEngines.Engines.FindPartialView(ControllerContext, viewName);
+                var viewContext = new ViewContext(ControllerContext, viewResult.View, ViewData, TempData, sw);
+                viewResult.View.Render(viewContext, sw);
+                viewResult.ViewEngine.ReleaseView(ControllerContext, viewResult.View);
+                return sw.GetStringBuilder().ToString();
+            }
+        }
+    }
+}
---
+++ Models/UserModel.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+using System.Web.Mvc;
+
+namespace HRMS.Models
+{
+    public class UserModel
+    {
+        [Display(Name = "id")]
+        public int id { get; set; }
+
+        [Remote("CheckUserName", "Account", ErrorMessage = "Duplicate User Name")]
+        [Required(ErrorMessage = "User Name is required.")]       
+        public string UserName { get; set; }
+
+        [Required(ErrorMessage = "Password is required.")]
+        public string Password { get; set; }
+
+        [Required(ErrorMessage = "First Name is required.")]
+        public string FirstName { get; set; }
+
+        [Required(ErrorMessage = "Last Name is required.")]
+        public string LastName { get; set; }
+
+        [Required(ErrorMessage = "Email is required.")]
+        [RegularExpression("^[a-zA-Z0-9_\\.-]+@([a-zA-Z0-9-]+\\.)+[a-zA-Z]{2,6}$", ErrorMessage = "E-mail is not valid")]
+        public string Email { get; set; }
+    }
+}
